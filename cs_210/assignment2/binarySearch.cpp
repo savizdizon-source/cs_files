739cd223d1154db8b3fb4841967a54ff1ecf0ec9
saviz <savizdizon@gmail.com>
2026-09-13 12:54:38 -0700

    Binary search adjustments

--- cs_210/assignment2/binarySearch.cpp
+++ cs_210/assignment2/binarySearch.cpp
@@ -29,15 +29,17 @@ int binarySearch_I(const int* numbers, int numberSize, int key) {
             return mid;
         }
 
-        return -1;
+        
     }
+    
+    return -1;
 }
 
 int main() {
     int numbers[] = {2, 4, 7, 10, 11, 32, 45, 87};
     const int numbersSize = sizeof(numbers) / sizeof(numbers[0]);
+    // Test at least five searches, including: first element, last element, middle element, missing value below the range, and missing value inside the range.
     int key = 7;
-
     int keyIndex1 = binarySearch_I(numbers, numbersSize, key);
     int keyIndex2 = binarySearch_R(numbers, 0, numbersSize - 1, key);
 
@@ -51,6 +53,22 @@ int main() {
     cout << "Key Index #2 (Recursive)" << endl;
     if (keyIndex2 == -1) {
         cout << key << "was not found." << endl;
-    }
+    } else {
+    cout << "Found " << key << " at index " << keyIndex2 << "." << endl;
+    }    
+
+    int keyFirstIndex = binarySearch_I(numbers, numbersSize, 2);
+    int keyLastIndex = binarySearch_I(numbers, numbersSize, 87);
+    int keyMiddleIndex = binarySearch_I(numbers, numbersSize, 11);
+    int keyBelowIndex = binarySearch_I(numbers, numbersSize, 1);
+    int keyAbove = binarySearch_I(numbers, numbersSize, 50);
+
+
+    cout << "Test first value: " << keyFirstIndex << endl;
+    cout << "Test last value: " << keyLastIndex << endl;
+    cout << "Test middle value: " << keyMiddleIndex << endl;
+    cout << "Test below range value: " << keyBelowIndex << endl;
+    cout << "Test above range value: " << keyAbove << endl;
+
 }
 
