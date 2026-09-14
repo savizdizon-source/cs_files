4efc955b3b1f6bb10a98ee1a729bea6b7786a493
saviz <savizdizon@gmail.com>
2026-09-13 17:24:20 -0700

    Selection sort algorithm

---
+++ cs_210/w03/selectionSort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> selectionSort(int* numbers, int numbersSize ) {
+    for (int i = 0; i < numbersSize - 1; i++) {
+        int smallestIndex = i;
+        for (int j = i + 1; j < numbersSize; j++) {
+            if (numbers[j] < numbers[smallestIndex]) {
+                smallestIndex = j;
+            }
+        }
+        int temp = numbers[i];
+        numbers[i] = numbers[smallestIndex];
+        numbers[smallestIndex] = temp;    
+    }
+}
+
+int main() {
+    vector<int> arr = {4, 12, 66, 1, 5, 17};
+    vector<int> sorted = selectionSort(arr.data(), arr.size());
+
+    for (auto& val : sorted) {
+        cout << val << endl;
+    }
+}
